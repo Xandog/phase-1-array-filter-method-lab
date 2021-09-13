5037bda77cde71cf0e571c948b091c6678946123
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-12 21:14:44 -0500

    commit tree ebb69f5e6c4cdd4485846ce7d0ab305f46c72ac7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { match } = require("sinon");
+
 // Code your solution here
+function findMatching(drivers, target){
+    const matchingDrivers = drivers.filter(drivers => drivers.toUpperCase() === target.toUpperCase());
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, target){
+    let index = 0;
+    const matchingDrivers = drivers.filter(drivers => drivers[index] === target[index])
+    return matchingDrivers;
+}
+
+function matchName(drivers, target){
+    const matchingDrivers = drivers.filter(drivers => drivers.name === target)
+    return matchingDrivers;
+}
